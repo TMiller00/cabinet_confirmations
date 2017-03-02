f23a218883c0e35bef5e3fe43051b5880790d027
TMiller00 <thomas.d.miller.jr@gmail.com>
2017-03-02 10:01:09 -0500

    Fixing my express

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-var express = require("express");
-var app     = express();
-var path    = require("path");
+var express = require('express');
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/css'));
+
+app.get('/', function(request, response) {
+  response.render('index.html');
 });
 
-app.use("/", express.static(__dirname + '/'));
-app.use("/css", express.static(__dirname + '/css'));
-
-app.listen(5000);
-
-
-
-console.log("Running at Port 5000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
